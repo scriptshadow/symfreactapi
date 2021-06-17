96eb4fadfd220ce24553ea0c7ddf104dc066dece
scriptshadow <scriptshadow2.0@gmail.com>
2021-06-17 22:58:26 +0200

    Préparation au déploiment.

--- assets/src/components/loaders/FormContentLoader.jsx
+++ assets/src/components/loaders/FormContentLoader.jsx
@@ -1,25 +1,24 @@
-import React from "react"
-import ContentLoader from "react-content-loader"
+import React from "react";
+import ContentLoader from "react-content-loader";
 
-const FormContentLoader = (props) => (
+const FormContentLoader = props => (
     <ContentLoader
-        speed={3}
-        width={500}
         height={500}
-        viewBox="0 0 500 500"
-        backgroundColor="#f3f3f3"
-        foregroundColor="#ecebeb"
+        width={800}
+        speed={2}
+        primaryColor="#f3f3f3"
+        secondaryColor="#ecebeb"
         {...props}
     >
-        <rect x="145" y="11" rx="0" ry="0" width="341" height="27" />
-        <rect x="63" y="84" rx="0" ry="0" width="158" height="22" />
-        <rect x="65" y="121" rx="0" ry="0" width="358" height="40" />
-        <rect x="67" y="265" rx="0" ry="0" width="158" height="22" />
-        <rect x="65" y="301" rx="0" ry="0" width="358" height="40" />
-        <rect x="66" y="176" rx="0" ry="0" width="158" height="22" />
-        <rect x="66" y="212" rx="0" ry="0" width="358" height="40" />
-        <rect x="70" y="392" rx="0" ry="0" width="165" height="52" />
-        <rect x="286" y="417" rx="0" ry="0" width="114" height="25" />
+        <rect x="0" y="10" rx="0" ry="0" width="151" height="15" />
+        <rect x="0" y="35" rx="0" ry="0" width="719" height="25" />
+        <rect x="0" y="80" rx="0" ry="0" width="151" height="15" />
+        <rect x="0" y="105" rx="0" ry="0" width="719" height="25" />
+        <rect x="0" y="150" rx="0" ry="0" width="151" height="15" />
+        <rect x="0" y="175" rx="0" ry="0" width="719" height="25" />
+        <rect x="0" y="220" rx="0" ry="0" width="151" height="15" />
+        <rect x="0" y="245" rx="0" ry="0" width="719" height="25" />
+        <rect x="0" y="290" rx="0" ry="0" width="130" height="30" />
     </ContentLoader>
-)
-export default FormContentLoader
+);
+export default FormContentLoader;
